959571b173f03a522fa9a1c7b57fedcd737e7b17
makejava <1353036300@qq.com>
2018-09-02 13:48:12 +0800

    重构克隆工具类，解决JSON序列化克隆方式不支持嵌套引用问题。

--- src/main/java/com/sjhy/plugin/tool/CloneUtils.java
+++ src/main/java/com/sjhy/plugin/tool/CloneUtils.java
@@ -1,10 +1,8 @@
 package com.sjhy.plugin.tool;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.intellij.util.ExceptionUtil;
 
-import java.io.IOException;
-import java.util.*;
+import java.io.*;
 
 /**
  * 克隆工具类，实现原理通过JSON序列化反序列化实现
@@ -13,30 +11,7 @@ import java.util.*;
  * @version 1.0.0
  * @since 2018/07/17 13:10
  */
-public class CloneUtils {
-    private volatile static CloneUtils cloneUtils;
-
-    /**
-     * 单例模式
-     */
-    public static CloneUtils getInstance() {
-        if (cloneUtils == null) {
-            synchronized (CloneUtils.class) {
-                if (cloneUtils == null) {
-                    cloneUtils = new CloneUtils();
-                }
-            }
-        }
-        return cloneUtils;
-    }
-
-    private CloneUtils() {
-    }
-
-    /**
-     * JSON处理工具类
-     */
-    private ObjectMapper objectMapper = new ObjectMapper();
+public final class CloneUtils {
 
     /**
      * 实体对象克隆方法
@@ -44,54 +19,33 @@ public class CloneUtils {
      * @param entity 实体对象
      * @return 克隆后的实体对象
      */
+    @SuppressWarnings("unchecked")
     public <E> E clone(E entity) {
         if (entity == null) {
             return null;
         }
-        try {
-            //noinspection unchecked
-            return objectMapper.readValue(objectMapper.writeValueAsString(entity), (Class<E>) entity.getClass());
-        } catch (IOException e) {
+        // 定义一个缓冲输出流对象
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        ObjectInputStream input = null;
+        try(ObjectOutputStream out = new ObjectOutputStream(buffer)) {
+            // 将对象输出到缓冲区
+            out.writeObject(entity);
+            // 重新从缓冲区读取对象
+            input = new ObjectInputStream(new ByteArrayInputStream(buffer.toByteArray()));
+            return (E) input.readObject();
+        } catch (IOException | ClassNotFoundException e) {
             ExceptionUtil.rethrow(e);
+        } finally {
+            // 关闭流
+            try {
+                buffer.close();
+                if (input != null) {
+                    input.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return null;
-    }
-
-    /**
-     * List集合克隆方法
-     *
-     * @param src 源数据
-     * @param <E> 实体类型
-     * @return 克隆结果
-     */
-    public <E> List<E> cloneList(List<E> src) {
-        if (src == null) {
-            return null;
-        }
-        List<E> result = new ArrayList<>(src.size());
-        src.forEach(e -> result.add(this.clone(e)));
-        return result;
-    }
-
-    /**
-     * Map集合克隆方法
-     *
-     * @param src 源数据
-     * @param <K> 键类型
-     * @param <E> 值类型
-     * @return 克隆结果
-     */
-    public <K, E> Map<K, E> cloneMap(Map<K, E> src) {
-        if (src == null) {
-            return null;
-        }
-        Map<K, E> result;
-        if (src instanceof LinkedHashMap) {
-            result = new LinkedHashMap<>(src.size());
-        } else {
-            result = new HashMap<>(src.size());
-        }
-        src.forEach((k, e) -> result.put(k, this.clone(e)));
-        return result;
     }
 }
