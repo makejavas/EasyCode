d27143956faabd7bdc0a11f94c938b35a37eef3b
makejava <1353036300@qq.com>
2021-02-01 17:53:20 +0800

    优化代码格式

--- src/main/java/com/sjhy/plugin/actions/ConfigAction.java
+++ src/main/java/com/sjhy/plugin/actions/ConfigAction.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import com.sjhy.plugin.ui.ConfigTableDialog;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -29,7 +30,7 @@ public class ConfigAction extends AnAction {
      * @param event 事件对象
      */
     @Override
-    public void actionPerformed(AnActionEvent event) {
+    public void actionPerformed(@NotNull AnActionEvent event) {
         Project project = event.getProject();
         if (project == null) {
             return;
