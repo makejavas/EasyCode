f3c904a0ee7268de1ceb71fcbd973ae641d7e6e8
makejava <1353036300@qq.com>
2022-10-13 10:37:35 +0800

    优化过时方法

--- src/main/java/com/sjhy/plugin/factory/AbstractItemFactory.java
+++ src/main/java/com/sjhy/plugin/factory/AbstractItemFactory.java
@@ -2,6 +2,8 @@ package com.sjhy.plugin.factory;
 
 import com.sjhy.plugin.entity.AbstractItem;
 
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * 抽象的项目工厂
  *
@@ -13,9 +15,9 @@ public class AbstractItemFactory {
 
     public static <T extends AbstractItem<T>> T createDefaultVal(Class<T> cls) {
         try {
-            T instance = cls.newInstance();
+            T instance = cls.getDeclaredConstructor().newInstance();
             return instance.defaultVal();
-        } catch (InstantiationException | IllegalAccessException e) {
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
             throw new IllegalArgumentException("构建示例失败", e);
         }
     }
